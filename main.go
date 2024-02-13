6d8401ff007c61059db71e3d80ba697189748e8c
ric-ram <ricram.log@gmail.com>
2024-02-13 01:01:32 +0000

    Add a custom handler

--- main.go
+++ main.go
@@ -18,12 +18,19 @@ func middlewareCors(next http.Handler) http.Handler {
 	})
 }
 
+func handleReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.HandleFunc("/healthz", handleReadiness)
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
 
 	corsMux := middlewareCors(mux)
 

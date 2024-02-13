8fee0aad24bc4aa7d3805620be25aad713d16639
ric-ram <ricram.log@gmail.com>
2024-02-13 00:38:45 +0000

    Add Chirpy logo

--- main.go
+++ main.go
@@ -19,10 +19,12 @@ func middlewareCors(next http.Handler) http.Handler {
 }
 
 func main() {
+	const filepathRoot = "."
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+
 	corsMux := middlewareCors(mux)
 
 	server := &http.Server{
@@ -30,7 +32,7 @@ func main() {
 		Handler: corsMux,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
 
 }

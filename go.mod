f1b2209d819685233a5d643cdc1b2dde43f359f1
ric-ram <ricram.log@gmail.com>
2024-02-13 00:17:20 +0000

    Setup basic server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ric-ram/go-chirpy
+
+go 1.21.6
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	const port = "8080"
+
+	mux := http.NewServeMux()
+	corsMux := middlewareCors(mux)
+
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: corsMux,
+	}
+
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(server.ListenAndServe())
+	// err := server.ListenAndServe()
+	// if err != nil {
+	// 	fmt.Printf("error listening for server: %s\n", err)
+	// }
+
+}
